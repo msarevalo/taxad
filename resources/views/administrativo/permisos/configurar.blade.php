cf1ca2593aa6380c2662a8b88a5f8d66eb9cabc4
Manuel Arevalo <msarevalo>
2020-03-23 14:41:25 -0500

    configuraciones

--- resources/views/administrativo/permisos/configurar.blade.php
+++ resources/views/administrativo/permisos/configurar.blade.php
@@ -28,37 +28,13 @@
 					{{$menu->nombre}}
 				</td>
 				<td>
-					@foreach($perfil as $per)
-						@if($per->menu==$menu->id)
-						<select style="text-transform: capitalize" name="{{$menu->nombre}}[]" class="form-control mb-2" multiple="multiple" required>
-							@if($per->permisos_menu==1)
-								<option style="text-transform: capitalize" value="verm-{{$menu->id}}" selected>Ver Menu</option>
-							@else
-								<option style="text-transform: capitalize" value="verm-{{$menu->id}}">Ver Menu</option>
-							@endif
-							@if($per->ver==1)
-		                    	<option style="text-transform: capitalize" value="ver-{{$menu->id}}" selected>Ver</option>
-		                    @else
-		                    	<option style="text-transform: capitalize" value="ver-{{$menu->id}}">Ver</option>
-		                    @endif
-		                    @if($per->crear==1)
-		                    	<option style="text-transform: capitalize" value="crear-{{$menu->id}}" selected>Crear</option>
-		                    @else
-		                    	<option style="text-transform: capitalize" value="crear-{{$menu->id}}">Crear</option>
-		                    @endif
-		                    @if($per->editar==1)
-		                    	<option style="text-transform: capitalize" value="editar-{{$menu->id}}" selected>Editar</option>
-		                    @else
-		                    	<option style="text-transform: capitalize" value="editar-{{$menu->id}}">Editar</option>
-		                    @endif
-		                    @if($per->eliminar==1)
-		                    	<option style="text-transform: capitalize" value="eliminar-{{$menu->id}}" selected>Eliminar</option>
-		                    @else
-		                    	<option style="text-transform: capitalize" value="eliminar-{{$menu->id}}">Eliminar</option>
-		                    @endif
-							</select>
-		            	@endif
-		            @endforeach
+					<select style="text-transform: capitalize" name="{{$menu->nombre}}[]" class="form-control mb-2" multiple="multiple" required>
+						<option style="text-transform: capitalize" value="verm-{{$menu->id}}">Ver Menu</option>
+		                <option style="text-transform: capitalize" value="ver-{{$menu->id}}">Ver</option>
+		                <option style="text-transform: capitalize" value="crear-{{$menu->id}}">Crear</option>
+		                <option style="text-transform: capitalize" value="editar-{{$menu->id}}">Editar</option>
+		                <option style="text-transform: capitalize" value="eliminar-{{$menu->id}}">Eliminar</option>
+		            </select>
 				</td>
 			</tr>
 		@endforeach
